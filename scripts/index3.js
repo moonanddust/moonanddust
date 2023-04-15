25d26018e5c9235d0d861fe545ffc986a196a105
Wang Yuchen <wangyuchen@WangdeAir.lan>
2023-04-15 15:49:51 +0800

    m

--- scripts/index3.js
+++ scripts/index3.js
@@ -1,6 +1,6 @@
 (function(){    //上一次见面的时间
     var box = document.getElementById('cdd');
-    var targetDate = new Date("2023/4/14 23:45:00");
+    var targetDate = new Date("2023/4/15 23:45:00");
     runTime();
     var intervalId = setInterval(runTime, 1000);
     function runTime() {
